1e4ca271e95cea3268adb7920d507b42cf66a924
Stefan Jacobs <stefan@zenaptix.com>
2018-11-16 11:36:49 +0100

    wild card commit

--- vars/deployPeripheral.groovy
+++ vars/deployPeripheral.groovy
@@ -1,6 +1,6 @@
 def call(String[] services, project) {
     services.each {
-        stage {
+        stage("Deploy $it") {
             return sh(script: "echo deploying $it in $project", returnStdout: true)
         }
     }
