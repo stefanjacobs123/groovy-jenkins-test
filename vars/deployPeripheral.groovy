a73b088f72b2d35bb4e152f12230a078b69947ff
Stefan Jacobs <stefan@zenaptix.com>
2018-11-16 12:39:43 +0100

    wild card commit

--- vars/deployPeripheral.groovy
+++ vars/deployPeripheral.groovy
@@ -1,7 +1,9 @@
 def call(String[] services, project) {
+    def deploy = libraryResource 'com/stefan/iit/deploy.sh'
+    writeFile file: 'deploy.sh', text: deploy
     services.each {
         stage("Deploy $it") {
-            return sh(script: "echo deploying $it in $project", returnStdout: true)
+            return sh(script: "source function.sh kafka", returnStdout: true)
         }
     }
 }
