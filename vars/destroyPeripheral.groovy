39d1bf959f345b16c2799bd7d79440c903b201eb
Stefan Jacobs <stefan@zenaptix.com>
2018-11-16 13:34:02 +0100

    Retry parallel stage in Jenkins.

--- vars/destroyPeripheral.groovy
+++ vars/destroyPeripheral.groovy
@@ -1,9 +1,7 @@
 def call(String[] services, project) {
     services.each {
-        this["Destroy $it"] = {
-            stage("Destroy $it") {
-                return sh(script: "./destroy.sh $it $project", returnStdout: true)
-            }
+        stage("Destroy $it") {
+            return sh(script: "./destroy.sh $it $project", returnStdout: true)
         }
     }
 }
