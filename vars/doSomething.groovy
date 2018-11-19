8bf0b5abe0e9a710fd9f440b1731ac9ff741f164
Stefan Jacobs <stefan@zenaptix.com>
2018-11-19 15:33:16 +0100

    Jeez

--- vars/doSomething.groovy
+++ vars/doSomething.groovy
@@ -1,5 +1,6 @@
 
 def call(File script) {
+    String workdir = pwd() - "/workspace"
     def loadScript = libraryResource script.path - workdir - "/workspace@libs/groovy-test/resources"
     writeFile file: script.name, text: loadScript
     println("Handling: ${script.name}")
