0802b162c4dbe4361ee30a22d254a1c87b0817fe
Stefan Jacobs <stefan@zenaptix.com>
2018-11-19 15:40:50 +0100

    Jeez wild

--- vars/doSomething.groovy
+++ vars/doSomething.groovy
@@ -1,8 +1,10 @@
-
-def call(File script) {
-    String workdir = pwd() - "/workspace"
-    def loadScript = libraryResource script.path - workdir - "/workspace@libs/groovy-test/resources"
-    writeFile file: script.name, text: loadScript
-    println("Handling: ${script.name}")
-    return sh(script: "chmod +x ${script.name}", returnStdout: true)
+def call(File[] scripts, workdir) {
+    scripts.each {
+        stage("Setup ${it.name}") {
+            def loadScript = libraryResource it.path - workdir - "/workspace@libs/groovy-test/resources"
+            writeFile file: it.name, text: loadScript
+            println("Handling: ${it.name}")
+            return sh(script: "chmod +x ${it.name}", returnStdout: true)
+        }
+    }
 }
--- vars/setupEnvironment.groovy
+++ vars/setupEnvironment.groovy
@@ -10,26 +10,7 @@ def call() {
     dir.traverse(type: FILES, maxDepth: 0) { script ->
         scripts.add(script)
     }
-//
-//    for(int i = 0; i < scripts.size(); i++) {
-//        def loadScript = libraryResource scripts[i].path - workdir - "/workspace@libs/groovy-test/resources"
-//        writeFile file: scripts[i].name, text: loadScript
-//        sh(script: "chmod +x ${scripts[i].name}", returnStdout: true)
-//    }
 
-//    scripts.forEach {
-//        println("%%%" + it)
-//        def loadScript = libraryResource it.path - workdir - "/workspace@libs/groovy-test/resources"
-//        writeFile file: it.name, text: loadScript
-//        println("Handling: ${it.name}")
-//        return sh(script: "chmod +x ${it.name}", returnStdout: true)
-//    }
-
-    scripts.each {
-        File script = it
-        stage("Copy ${script.name}") {
-            doSomething(script)
-        }
-    }
+    doSomething(scripts, workdir)
 }
 
