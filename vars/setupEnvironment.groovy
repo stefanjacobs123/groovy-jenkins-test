2d63b506a7146d72176881b5998a20665c056150
Stefan Jacobs <stefan@zenaptix.com>
2018-11-19 13:18:32 +0100

    Change dir structure for Jenkins.

--- vars/setupEnvironment.groovy
+++ vars/setupEnvironment.groovy
@@ -2,9 +2,9 @@ import groovy.io.FileType
 
 def call() {
 
-    def dir = new File("resources/com/stefan/iit")
+    def dir = new File("./resources/com/stefan/iit")
 
-    dir.eachFileRecurse (FileType.ANY) { File script ->
+    dir.eachFileRecurse (FileType.FILES) { File script ->
         def loadScript = libraryResource script.path - "resources/"
         writeFile file: script.name, text: loadScript
         sh "chmod +x ${script.name}"
