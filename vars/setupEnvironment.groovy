7e4acbe0ee17ff8ee8ba0db3916bb4a4e4eb0e83
Stefan Jacobs <stefan@zenaptix.com>
2018-11-19 15:12:33 +0100

    wild card

--- vars/setupEnvironment.groovy
+++ vars/setupEnvironment.groovy
@@ -5,10 +5,13 @@ def call() {
     String workdir = pwd() - "/workspace"
     def dir = new File(workdir + "/workspace@libs/groovy-test/resources/com/stefan/iit")
 
-    def scripts = []
+//    def scripts = []
 
     dir.traverse(type: FILES, maxDepth: 0) { script ->
-        scripts.add(script)
+        def loadScript = libraryResource script.path - workdir - "/workspace@libs/groovy-test/resources"
+        writeFile file: script.name, text: loadScript
+        println("Handling: ${script.name}")
+        return sh(script: "chmod +x ${script.name}", returnStdout: true)
     }
 
 //    for(int i = 0; i < scripts.size(); i++) {
@@ -19,13 +22,13 @@ def call() {
 //        return sh(script: "chmod +x ${scripts[i].name}", returnStdout: true)
 //    }
 
-    scripts.forEach {
-        println("%%%" + it)
-        def loadScript = libraryResource it.path - workdir - "/workspace@libs/groovy-test/resources"
-        writeFile file: it.name, text: loadScript
-        println("Handling: ${it.name}")
-        return sh(script: "chmod +x ${it.name}", returnStdout: true)
-    }
+//    scripts.forEach {
+//        println("%%%" + it)
+//        def loadScript = libraryResource it.path - workdir - "/workspace@libs/groovy-test/resources"
+//        writeFile file: it.name, text: loadScript
+//        println("Handling: ${it.name}")
+//        return sh(script: "chmod +x ${it.name}", returnStdout: true)
+//    }
 
 //    scripts.each {
 //        File file = it
