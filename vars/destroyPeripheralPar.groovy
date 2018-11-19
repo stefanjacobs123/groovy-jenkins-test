c21f14320c2780b8ca8e5a042323072f9f4b1680
Stefan Jacobs <stefan@zenaptix.com>
2018-11-19 18:00:37 +0100

    Curlies are important. Otherwise, emptyMap is key->actaul execution

--- vars/destroyPeripheralPar.groovy
+++ vars/destroyPeripheralPar.groovy
@@ -1,7 +1,7 @@
 def call(String[] services, project, templatesVersion) {
     def emptyMap = [:]
     services.each {
-        emptyMap.put("Destroy " + it, sh(script: "./destroy.sh $it $project $templatesVersion") )
+        emptyMap.put("Destroy " + it, { sh(script: "./destroy.sh $it $project $templatesVersion") } )
     }
     parallel(emptyMap)
 }
