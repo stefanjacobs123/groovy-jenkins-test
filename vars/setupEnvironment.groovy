b06d58ad51eb6fd60709b46f01f8fd046e2f3af7
Stefan Jacobs <stefan@zenaptix.com>
2018-11-19 14:00:22 +0100

    Change dir structure for Jenkins.

--- vars/setupEnvironment.groovy
+++ vars/setupEnvironment.groovy
@@ -2,8 +2,8 @@ import groovy.io.FileType
 
 def call() {
 
-    String workdir = pwd()
-    def dir = new File(workdir + "../workspace@libs/groovy-test/resources/com/stefan/iit")
+    String workdir = pwd() - "/workspace"
+    def dir = new File(workdir + "/workspace@libs/groovy-test/resources/com/stefan/iit")
 
     dir.eachFileRecurse (FileType.FILES) { File script ->
         def loadScript = libraryResource script.path - "resources/"
