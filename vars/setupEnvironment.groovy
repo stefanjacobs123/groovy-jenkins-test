d0bf5dbb48e8d6db281b318ab027f536db0a0df8
Stefan Jacobs <stefan@zenaptix.com>
2018-11-19 14:40:06 +0100

    wild card

--- vars/setupEnvironment.groovy
+++ vars/setupEnvironment.groovy
@@ -1,12 +1,12 @@
 import groovy.io.FileType
 
-@NonCPS
+@NonCPS //https://issues.jenkins-ci.org/browse/JENKINS-26481
 def call() {
 
     String workdir = pwd() - "/workspace"
     def dir = new File(workdir + "/workspace@libs/groovy-test/resources/com/stefan/iit")
 
-    dir.eachFileRecurse (FileType.FILES) { File script ->
+    dir.traverse (type: FileType.FILES) { File script ->
         def loadScript = libraryResource script.path - workdir - "/workspace@libs/groovy-test/resources"
         writeFile file: script.name, text: loadScript
         println("Handling: ${script.name}")
