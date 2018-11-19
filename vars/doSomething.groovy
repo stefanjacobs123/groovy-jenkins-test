c18daeb8d911301feae7902d146ac1bfa6665721
Stefan Jacobs <stefan@zenaptix.com>
2018-11-19 17:39:58 +0100

    don't wrap in stages

--- vars/doSomething.groovy
+++ vars/doSomething.groovy
@@ -1,9 +1,9 @@
 def call(List<File> scripts, String workDir) {
     scripts.each {
-        stage("Setup ${it.name}") {
+        //stage("Setup ${it.name}") {
             def loadScript = libraryResource it.path - workDir - "/workspace@libs/groovy-test/resources"
             writeFile file: it.name, text: loadScript
             return sh(script: "chmod +x ${it.name}", returnStdout: true)
-        }
+        //}
     }
 }
