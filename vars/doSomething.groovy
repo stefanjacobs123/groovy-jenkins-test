f516381acd608d08b3aba648ecac93f233fbc764
Stefan Jacobs <stefan@zenaptix.com>
2018-11-19 15:33:38 +0100

    Jeez

--- vars/doSomething.groovy
+++ vars/doSomething.groovy
@@ -4,5 +4,5 @@ def call(File script) {
     def loadScript = libraryResource script.path - workdir - "/workspace@libs/groovy-test/resources"
     writeFile file: script.name, text: loadScript
     println("Handling: ${script.name}")
-    return sh(script: "chmod +x ${it.name}", returnStdout: true)
+    return sh(script: "chmod +x ${script.name}", returnStdout: true)
 }
