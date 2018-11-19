99ad099129d7734af62011a5e51026daaeec09a5
Stefan Jacobs <stefan@zenaptix.com>
2018-11-19 14:37:30 +0100

    wild card

--- vars/setupEnvironment.groovy
+++ vars/setupEnvironment.groovy
@@ -1,33 +1,16 @@
-import static groovy.io.FileType.FILES
+import groovy.io.FileType
+
 @NonCPS
 def call() {
 
     String workdir = pwd() - "/workspace"
     def dir = new File(workdir + "/workspace@libs/groovy-test/resources/com/stefan/iit")
 
-    def scripts = []
-
-    dir.traverse(type: FILES, maxDepth: 0) { script ->
-        scripts.add(script)
+    dir.eachFileRecurse (FileType.FILES) { File script ->
+        def loadScript = libraryResource script.path - workdir - "/workspace@libs/groovy-test/resources"
+        writeFile file: script.name, text: loadScript
+        println("Handling: ${script.name}")
+        sh "chmod +x ${script.name}"
     }
 
-    def countFilesAndDirs = 0
-    dir.traverse {
-        countFilesAndDirs++
-    }
-
-    println("&&&" + countFilesAndDirs)
-
-    scripts.each {
-        println("@@@" + it)
-    }
-
-    scripts.each {
-        File file = it
-        def loadScript = libraryResource file.path - workdir - "/workspace@libs/groovy-test/resources"
-        writeFile file: file.name, text: loadScript
-        println("Handling: ${file.name}")
-        sh "chmod +x ${file.name}"
-    }
 }
-
