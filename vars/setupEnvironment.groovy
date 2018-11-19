e0fbda163c3cb4d099bff14ee9ad0179cd5049d0
Stefan Jacobs <stefan@zenaptix.com>
2018-11-19 15:42:16 +0100

    Jeez wild

--- vars/setupEnvironment.groovy
+++ vars/setupEnvironment.groovy
@@ -5,9 +5,9 @@ def call() {
     String workdir = pwd() - "/workspace"
     def dir = new File(workdir + "/workspace@libs/groovy-test/resources/com/stefan/iit")
 
-    def scripts = []
+    def scripts = File[]
 
-    dir.traverse(type: FILES, maxDepth: 0) { script ->
+    dir.traverse(type: FILES, maxDepth: 0) { File script ->
         scripts.add(script)
     }
 
