c0b83f199b24b725fa1644b860b9e3c632beaf48
Stefan Jacobs <stefan@zenaptix.com>
2018-11-19 17:18:50 +0100

    Wild card: return map stageName to script

--- vars/destroyPeripheral.groovy
+++ vars/destroyPeripheral.groovy
@@ -1,7 +1,7 @@
 def call(String[] services, project, templatesVersion) {
     def emptyMap = [:]
     services.each {
-        emptyMap.put(it, sh(script: "./destroy.sh $it $project $templatesVersion", returnStdout: true))
+        emptyMap.put(it, { sh(script: "./destroy.sh $it $project $templatesVersion", returnStdout: true) } )
     }
     return emptyMap
 }
