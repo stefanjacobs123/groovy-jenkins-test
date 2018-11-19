e3df0d60a104b84efb8bfce40887611380107154
Stefan Jacobs <stefan@zenaptix.com>
2018-11-19 16:38:36 +0100

    parallel wild card

--- vars/destroyPeripheral.groovy
+++ vars/destroyPeripheral.groovy
@@ -1,11 +1,6 @@
 def call(String[] services, project, templatesVersion) {
-    def destroyPeripheral = [failFast: true]
-    services.each {
-        destroyPeripheral["Destroy $it"] = {
-            stage("Destroy $it") {
-                return sh(script: "./destroy.sh $it $project $templatesVersion", returnStdout: true)
-            }
-        }
+    return stage("Destroy $it") {
+        return sh(script: "./destroy.sh $it $project $templatesVersion", returnStdout: true)
     }
 }
 
